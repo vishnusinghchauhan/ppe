c8b27bd3bf5e663b7b9dee82b2cbdc2fa8414efc
vishnu.chauhan <vishnu.chauhan@espire.com>
2020-08-25 15:38:35 +0530

    Authenticate added in APIs

--- routes/stocks.js
+++ routes/stocks.js
@@ -4,19 +4,19 @@ const router = express.Router();
 const middleware = require('./middleware');
 const stockController = require('../controllers/stockController');
 
-router.post('/addSingleProduct',  stockController.addSingleProduct)
+router.post('/addSingleProduct',  middleware.checkToken, stockController.addSingleProduct)
 
-router.put('/getProductList',  stockController.getProductList)
+router.put('/getProductList', middleware.checkToken,  stockController.getProductList)
 
-router.get('/getProductDetail/:productId',  stockController.getProductDetail)
+router.get('/getProductDetail/:productId', middleware.checkToken,  stockController.getProductDetail)
 
-router.get('/sendInquiry/:productId',  stockController.sendProductInquiry)
+router.get('/sendInquiry/:productId', middleware.checkToken, stockController.sendProductInquiry)
 
-router.put('/getPendingProducts',  stockController.getAllPendingProducts)
+router.put('/getPendingProducts', middleware.checkToken,  stockController.getAllPendingProducts)
 
-router.get('/getAllCompanies',  stockController.getAllCompanies)
+router.get('/getAllCompanies', middleware.checkToken, stockController.getAllCompanies)
 
-router.post('/upload',  stockController.uploadImage)
+router.post('/upload',  middleware.checkToken, stockController.uploadImage)
 
 
 
